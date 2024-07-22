16f79f170300584d478c42c85f6e591bac86d111
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-22 11:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('submit').addEventListener('click', function() {
-    const player1 = document.getElementById('player-1').value;
-    const player2 = document.getElementById('player-2').value;
+    const player1 = document.getElementById('player1').value;
+    const player2 = document.getElementById('player2').value;
 
     if (player1 && player2) {
         document.getElementById('player-input').style.display = 'none';
